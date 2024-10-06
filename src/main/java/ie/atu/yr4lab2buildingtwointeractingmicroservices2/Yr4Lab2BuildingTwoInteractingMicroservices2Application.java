1a0cbfc7590a943225f26cf25e9f8a42fec82a3f
Mikaela-D <G00401392@atu.ie>
2024-10-06 16:14:04 +0100

    Updated pom file and set the port number

--- src/main/java/ie/atu/yr4lab2buildingtwointeractingmicroservices2/Yr4Lab2BuildingTwoInteractingMicroservices2Application.java
+++ src/main/java/ie/atu/yr4lab2buildingtwointeractingmicroservices2/Yr4Lab2BuildingTwoInteractingMicroservices2Application.java
@@ -2,8 +2,10 @@ package ie.atu.yr4lab2buildingtwointeractingmicroservices2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class Yr4Lab2BuildingTwoInteractingMicroservices2Application {
 
     public static void main(String[] args) {
